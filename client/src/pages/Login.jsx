d87366c99ad8f69eef10fb0331d1428028c30f25
Yash <yashvardhandubey83@gmail.com>
2026-04-10 19:25:16 +0530

    updated code

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,147 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+import { AppContent } from "../context/AppContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  return <div></div>;
+  const [state, setState] = useState("Sign up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const { backendUrl, setIsLoggedIn } = useContext(AppContent);
+
+  const onSubmitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true;
+      if (state === "Sign up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedIn(true);
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/auth/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedIn(true);
+          navigate("/");
+        } else {
+          toast.error(data.message);
+        }
+      }
+    } catch (error) {
+      toast.error(data.message);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen px-6  sm:px-0 bg-linear-to-br from-blue-200 to-purple-400">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer "
+      />
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign up" ? "Create Accout " : "Login "}
+        </h2>
+        <p className="text-center text-sm mb-6 ">
+          {state === "Sign up" ? "Create your accout " : "Login your Account"}
+        </p>
+
+        <form onSubmit={onSubmitHandler}>
+          {state === "Sign up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5  rounded-full bg-[#333A5c]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                type="text"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="bg-transparent outline-none"
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5  rounded-full bg-[#333A5c]">
+            <img src={assets.mail_icon} alt="" />
+            <input
+              type="=email"
+              placeholder="Email Id"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="bg-transparent outline-none"
+            />
+          </div>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5  rounded-full bg-[#333A5c]">
+            <img src={assets.lock_icon} alt="" />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="bg-transparent outline-none"
+            />
+          </div>
+
+          <p
+            onClick={() => navigate("/reset-password")}
+            className="mb-4 text-indigo-500 cursor-pointer "
+          >
+            Forgot Password
+          </p>
+
+          <button className="w-full py-2.5 rounded-full bg-linear-to-r from-indigo-500 to-indigo-900 text-white font-medium">
+            {state}
+          </button>
+        </form>
+
+        {state === "Sign up" ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-400 cursor-pointer underline "
+            >
+              Login Here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{" "}
+            <span
+              onClick={() => setState("Sign up")}
+              className="text-blue-400 cursor-pointer underline "
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
